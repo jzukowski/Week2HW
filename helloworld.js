c776f46f22a0d2526823e65e5125edfbc44dc19b
jzukowski <jan_zukowski@verizon.net>
2022-09-08 06:33:35 -0400

    update made for week 3 assignment

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Hi there! This instance was built by Jan for SWDV 660.\nIf you see this then the web server is running!\n')
+   response.end('Hi there! This instance was built by Jan for SWDV 660.\nIf you see this then the web server is running!\nUpdate made for week 3\n')
 }).listen(3333)
 
 // Console will print the message
